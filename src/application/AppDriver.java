43cbf9f5e93606da8d5053e74bbfa5367246ff2a
Ghassan Alnajjar <ghassan.f.najjar@gmail.com>
2024-02-23 14:45:18 -0700

    AppDriver preq.

--- src/application/AppDriver.java
+++ src/application/AppDriver.java
@@ -1,5 +1,11 @@
 package application;
 
+import controller.AppManager;
+
 public class AppDriver {
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to my shop!");
+        AppManager app = new AppManager();
+    }
 }
